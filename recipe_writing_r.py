8d3a1dc73e1dcffd43cd68b56867842d99f6b522
Olga Belitskaya <safuolga@gmail.com>
2020-12-01 02:37:12 +0300

    update recipe-writing-r.ipynb

--- recipe_writing_r.py
+++ recipe_writing_r.py
@@ -126,21 +126,21 @@ conn<-file("aggr_csv.R")
 writeLines("#unlock for running in SageMathCell 
 #%%r
 mean_aggr_csv<-function(url,sep,column,group_by) {
-    df<-read.csv(url,header=TRUE,sep='\t')
+    df<-read.csv(url,header=TRUE,sep=sep)
     len<-dim(df)[1]
     group_list<-list(df[1:len,group_by])
     result<-aggregate(df[column],group_list,mean)
     colnames(result)<-c(group_by,paste0('mean ',column))
     return(result)}
 median_aggr_csv<-function(url,sep,column,group_by) {
-    df<-read.csv(url,header=TRUE,sep='\t')
+    df<-read.csv(url,header=TRUE,sep=sep)
     len<-dim(df)[1]
     group_list<-list(df[1:len,group_by])
     result<-aggregate(df[column],group_list,median)
     colnames(result)<-c(group_by,paste0('median ',column))
     return(result)}
 sum_aggr_csv<-function(url,sep,column,group_by) {
-    df<-read.csv(url,header=TRUE,sep='\t')
+    df<-read.csv(url,header=TRUE,sep=sep)
     len<-dim(df)[1]
     group_list<-list(df[1:len,group_by])
     result<-aggregate(df[column],group_list,sum) 
